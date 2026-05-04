5b0e8cfcf5c8703655280cd58f48f13965cc8c02
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2026-05-04 13:38:52 +0900

    feat: ゲインのランダム化を有効化

--- source/isaaclab_k1_locomotion/isaaclab_k1_locomotion/tasks/manager_based/locomotion/velocity_env_cfg.py
+++ source/isaaclab_k1_locomotion/isaaclab_k1_locomotion/tasks/manager_based/locomotion/velocity_env_cfg.py
@@ -192,17 +192,17 @@ class EventCfg:
         },
     )
 
-    # randomize_actuator_gains = EventTerm(
-    #     func=mdp.randomize_actuator_gains,
-    #     mode="startup",
-    #     params={
-    #         "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
-    #         "stiffness_distribution_params": (0.8, 1.2),
-    #         "damping_distribution_params": (0.8, 1.2),
-    #         "operation": "scale",
-    #         "distribution": "uniform",
-    #     },
-    # )
+    randomize_actuator_gains = EventTerm(
+        func=mdp.randomize_actuator_gains,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
+            "stiffness_distribution_params": (0.8, 1.2),
+            "damping_distribution_params": (0.8, 1.2),
+            "operation": "scale",
+            "distribution": "uniform",
+        },
+    )
 
     # reset
     base_external_force_torque = EventTerm(
