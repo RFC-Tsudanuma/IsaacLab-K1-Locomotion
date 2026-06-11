8f7609c9196ea75e4194445bb5ab57500c3b4ddb
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2026-06-11 15:26:43 +0900

    feat: add scripts

---
+++ scripts/rsl_rl/tensor.sh
@@ -0,0 +1,2 @@
+source ~/.bash_functions
+_labpython2 -m tensorboard.main --logdir logs/rsl_rl/k1_flat $@
--- scripts/rsl_rl/train.sh
+++ scripts/rsl_rl/train.sh
@@ -1,2 +1,2 @@
 source /home/satoshi/.bash_functions
-_labpython2 train.py --task Isaac-Velocity-Flat-K1-v0 --headless --num_envs 8096 $@
+_labpython2 train.py --task Isaac-Velocity-Flat-K1-v0 --headless --num_envs 4096 $@
