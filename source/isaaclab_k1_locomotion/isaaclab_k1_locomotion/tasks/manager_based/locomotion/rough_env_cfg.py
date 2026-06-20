5612f1b2317007f1ddd35cc99462b37011cc2e74
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2026-06-20 19:22:58 +0900

    feat: 0.05だと足が内か外に曲がりすぎるので増やした

--- source/isaaclab_k1_locomotion/isaaclab_k1_locomotion/tasks/manager_based/locomotion/rough_env_cfg.py
+++ source/isaaclab_k1_locomotion/isaaclab_k1_locomotion/tasks/manager_based/locomotion/rough_env_cfg.py
@@ -310,7 +310,7 @@ class K1Rewards(RewardsCfg):
 
     joint_deviation_hip = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.05,
+        weight=-0.12,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*_Hip_Yaw", ".*_Hip_Roll"])},
     )
     # joint_deviation_arm = RewTerm(
